8c58364d67be8a50a7913d79e24e19a430a34b5d
Parimeth Wongsatayanon <jame2003mvp@hotmail.com>
2015-12-16 04:45:42 +0700

    Intro UI and Fader

---
+++ Assets/Scripts/LoadLevelManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadLevelManager : MonoBehaviour {
+
+//	private ScreenFadeInOut screen;
+	public GameObject Image;
+	bool isLoading;
+
+	void Start(){
+		
+	}
+
+	void Awake(){
+		isLoading = false;
+		//screen = ScreenFadeInOut.Instance;
+//		screen.FadeToBlack ();
+	}
+
+	public void ChangeLevel(string levelName){
+		if (isLoading)
+			return;
+		Image.GetComponent<Animator>().SetBool("isFade",true);
+		StartCoroutine (loadLevel (levelName));
+
+	}
+
+	IEnumerator loadLevel(string levelName)
+	{
+		isLoading = true;
+		yield return new WaitForSeconds (1.0f);
+		Application.LoadLevel (levelName);
+	}
+
+}
